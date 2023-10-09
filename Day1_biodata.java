687eceff22a4faa434a79359bbf11ca14b0f5c44
Serli30 <tiaa14596@gmail.com>
2023-10-09 08:02:32 +0800

    Day1_biodata

---
+++ Day1_biodata.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day1_biodata;
+
+/**
+ *
+ * @author HP
+ */
+public class Day1_biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nama   : Serli");
+        System.out.println("Nim    : D0223024");
+        System.out.println("Kelas  : Informatika C");
+        System.out.println("TTL    : Kalukunangka,28-07-2006");
+        System.out.println("Alamat :Pamboang");
+    }
+    
+}
