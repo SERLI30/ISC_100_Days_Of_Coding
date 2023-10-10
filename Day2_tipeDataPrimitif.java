ddf8c29215b4fcc6cb0b77344dde3a903a469132
Serli30 <tiaa14596@gmail.com>
2023-10-10 10:59:32 +0800

    Day2_tipeDataPrimitif

---
+++ Day2_tipeDataPrimitif.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day2_tipedataprimitif;
+
+/**
+ *
+ * @author HP
+ */
+public class Day2_tipeDataPrimitif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte data = 10;
+        int  umur = 28;
+        char li ='A';
+        float nilai = 8.5f;
+        double pi = 3.345;
+        long tinggi = 175;
+        short usia = 17;
+        boolean hasil = false;
+        
+       
+    }
+    
+}
