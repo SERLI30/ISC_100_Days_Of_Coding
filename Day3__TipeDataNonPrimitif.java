f5f1b84adafb4a9baeb4c732433f120abd20adba
Serli30 <tiaa14596@gmail.com>
2023-10-11 11:41:00 +0800

    Day3__TipeDataNonPrimitif

---
+++ Day3__TipeDataNonPrimitif.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day3_tipedatanonprimitif;
+
+/**
+ *
+ * @author HP
+ */
+public class Day3__TipeDataNonPrimitif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String nama= "Serli";
+    int[] prima = new int[10]; //sebuah array yang memuat 10 integer
+    int A =1; int B=2; int C =3; int D=4;
+    
+    
+    }
+    }
