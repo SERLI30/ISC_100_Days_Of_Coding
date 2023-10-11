61f3042ed62beed9acd9f00b90b387cfc1836b94
Serli30 <tiaa14596@gmail.com>
2023-10-11 10:57:23 +0800

    Day3_TipeDataNonPrimitif

---
+++ Day3_TipeDataNonPrimitif/src/day3_tipedatanonprimitif/Day3_TipeDataNonPrimitif.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day3_tipedatanonprimitif;
+
+/**
+ *
+ * @author HP
+ */
+public class Day3_TipeDataNonPrimitif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
