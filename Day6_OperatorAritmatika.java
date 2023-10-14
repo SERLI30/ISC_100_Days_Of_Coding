280e2735f3384a339dc0b539c480dfd9ded8a866
Serli30 <tiaa14596@gmail.com>
2023-10-14 11:02:29 +0800

    Day6_OperatorAritmatika

---
+++ Day6_OperatorAritmatika.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day6_operatoraritmatika;
+
+/**
+ *
+ * @author HP
+ */
+public class Day6_OperatorAritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int r = 28;
+    int s = 7;
+    int p = 05;
+    int l = 12;
+    int a = 9;
+    int t = 5;
+    double phi = 30.14;
+    
+    double lingkaran = phi * r * r;
+    int persegi = s * s;
+    int persegipanjang = p * l;
+    int segitiga = a * t;
+    
+        System.out.println("luas lingkaran ialah: " + lingkaran);
+        System.out.println("luas persegi ialah: " + persegi);
+        System.out.println("luas segitiga ialah: " + segitiga );
+        System.out.println("luas persegi panjang ialah: " + persegipanjang);
+    
+    }
+    
+}
