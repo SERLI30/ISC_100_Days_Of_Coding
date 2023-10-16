1aef72956a64fef7909915d4c8b8e04681025f2b
Serli30 <tiaa14596@gmail.com>
2023-10-16 10:54:33 +0800

    Day8_OperatorPembanding

---
+++ Day8_OperatorPembanding.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day8_operatorpenugasan;
+
+/**
+ *
+ * @author HP
+ */
+public class Day8_OperatorPenugasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int umur = 18;
+    int umur1 = 27;
+    boolean hasil;
+        hasil = umur == umur1;
+        System.out.println("umur == umur1 : " + hasil);
+        hasil = umur != umur1;
+        System.out.println("umur != umur1 : " + hasil);
+        hasil = umur < umur1;
+        System.out.println("umur < umur1 : " + hasil);
+        hasil = umur > umur1;
+        System.out.println("umur > umur1 : " + hasil);
+        hasil = umur >= umur1;
+        System.out.println("umur >= umur1 : " + hasil);
+        hasil = umur <= umur1;
+        System.out.println("umur <= umur1 : " + hasil);
+    }   
+    
+}
