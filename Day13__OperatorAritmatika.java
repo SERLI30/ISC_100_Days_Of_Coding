c4b1772f0cdd3d42fe623ccf63971f7780c72122
Serli30 <tiaa14596@gmail.com>
2023-10-21 22:13:59 +0800

    Day13__OperatorAritmatika

---
+++ Day13__OperatorAritmatika.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day13__operatoraritmatika;
+
+/**
+ *
+ * @author HP
+ */
+public class Day13__OperatorAritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a,b,c,d,e,f;
+    a = 28 - 7;
+    b = 7 * 2;
+    c = 30 / 5 ;
+    d = 10 + 6;
+    e = 95 % 7;
+    f = 28 * 7 - 2 + 5 / 95 % 30;
+    
+    
+        System.out.println("jawaban a:" + a);
+        System.out.println("jawaban b:" + b);
+        System.out.println("jawaban c:" + c);
+        System.out.println("jawaban d:" + d);
+        System.out.println("jawaban e:" + e);
+        System.out.println("jawaban f:" + f);
+    }
+    
+}
