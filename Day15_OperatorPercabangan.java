ec5c6e2c615937a86929c4bc938bd75fd7c9a835
Serli30 <tiaa14596@gmail.com>
2023-10-23 12:57:36 +0800

    Day15_OperatorPercabangan

---
+++ Day15_OperatorPercabangan.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day15_operatorpercabangan;
+
+/**
+ *
+ * @author HP
+ */
+public class Day15_OperatorPercabangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int a = 17;
+    if (a == 17){
+        System.out.println("angka yang di masukkan benar");
+    }else{
+        System.out.println("nilainya tidak di ketahui");
+        
+        
+        }
+       
+    }
+    
+    }
+    }
+    
+
