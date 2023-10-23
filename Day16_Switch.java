4df97eb3e9277cdde967b9a3e7d1b4ddfc4e46af
Serli30 <tiaa14596@gmail.com>
2023-10-24 06:25:16 +0800

    Day16_Switch

---
+++ Day16_Switch.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day16_switch;
+
+/**
+ *
+ * @author HP
+ */
+public class Day16_Switch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int hari =4;
+    switch (hari){
+        case 1:
+            System.out.println("hari ini hari sabtu");
+            break;
+        case 4:
+            System.out.println("hari ini hari rabu");
+            break;
+        
+        
+    }
+    }
+    
+}
