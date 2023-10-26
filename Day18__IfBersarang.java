97679f2ba0cd0eb7f92447f66ce3de5ebc798e88
Serli30 <tiaa14596@gmail.com>
2023-10-26 13:50:31 +0800

    Day18__IfBersarang

---
+++ Day18__IfBersarang.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day18__ifbersarang;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Day18__IfBersarang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner serli = new Scanner(System.in);
+        System.out.println("masukkan usia");
+        int usia = serli.nextInt();
+        if(usia >= 20){
+            System.out.println("dewasa");
+        } else {
+            if(usia >= 15){
+        }else{
+                System.out.println("anak-anak");
+            }
+        }
+        
+    }
+    
+}
