2bdc2117c2439c120a578834bff36cce7768edec
Serli30 <tiaa14596@gmail.com>
2023-11-01 10:19:12 +0800

    Day24_DoWhile

---
+++ Day24_DoWhile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day24_dowhile;
+
+/**
+ *
+ * @author HP
+ */
+public class Day24_DoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   int i = 0;
+   do{
+       System.out.println("hai teman-teman");
+       i++;
+   }
+   while(i <= 4);
+    }
+    
+}
