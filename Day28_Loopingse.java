9a76b89b239b2b5736f9966fe89c8f2069e8c519
Serli30 <tiaa14596@gmail.com>
2023-11-05 12:04:22 +0800

    Day28_Loopingse

---
+++ Day28_Loopingse.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day28_loopingse;
+
+/**
+ *
+ * @author HP
+ */
+public class Day28_Loopingse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 10; j++){
+                System.out.print("*");
+    }
+        System.out.print("\n");
+}
+    }
+}
