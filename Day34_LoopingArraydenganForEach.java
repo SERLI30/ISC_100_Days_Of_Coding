f3ed33941818875796b4969d35fef59404bc0489
Serli30 <tiaa14596@gmail.com>
2023-11-12 10:33:39 +0800

    Day34_LoopingArraydenganForEach

---
+++ Day34_LoopingArraydenganForEach.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day34_loopingarraydenganforeach;
+
+/**
+ *
+ * @author HP
+ */
+public class Day34_LoopingArraydenganForEach {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String[]memberBTS={"NAMJOON","JIN","J-HOPE","SUGA","JIMIN","TAEHYUNG","JUNGKOOK"};
+        for (String i:memberBTS) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
+
+
