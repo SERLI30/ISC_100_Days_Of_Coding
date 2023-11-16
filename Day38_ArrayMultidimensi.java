dd7089c98a57847b8c85d43cc3d1091d46d54088
Serli30 <tiaa14596@gmail.com>
2023-11-17 00:03:59 +0800

    Day39_Array..

--- Day38_ArrayMultidimensi.java
+++ Day38_ArrayMultidimensi.java
@@ -15,22 +15,42 @@ public class Day38_ArrayMultidimensi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     int [][] array_2D = {
-         {11,12,13,14,15},
-         {16,17,18,19,20}
-          
-     };
-   printArray2D(array_2D);
-       
-int [][] arrayAngka = new int [3][5];
-
- printArray2D(arrayAngka);
- 
- }
-  private static void printArray2D(int[][] dataArray) {
- for(int i = 0; i < dataArray.length; i++) {
-          
-             System.out.println(Arrays.toString(dataArray[i]));
+//     int [][] array_2D = {
+//         {11,12,13,14,15},
+//         {16,17,18,19,20}
+//          
+//     };
+//   printArray2D(array_2D);
+//       
+//int [][] arrayAngka = new int [3][5];
+//
+// printArray2D(arrayAngka);
+// 
+// }
+//  private static void printArray2D(int[][] dataArray) {
+// for(int i = 0; i < dataArray.length; i++) {
+//          
+//             System.out.println(Arrays.toString(dataArray[i]));
+Scanner serli = new Scanner(System.in);
+        int s = serli.nextInt();
+        int[]l =new int[s];
+        int a = 0;
+        int b = 5;
+        
+        for (int i = 0; i < l.length; i++) {
+            if(s % 5 == 0){
+                if(b% 5 == 0){
+                    System.out.println(l[i]+b);
+                    b++;
+            
+        }
+                b++;
+            }else if(s % 5 != 0){
+                a++;
+            if(a % 5 != 0)
+                    System.out.println(l[i]+a);
+            }
+            a++;
    }
         
     }
