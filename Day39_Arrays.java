ebfa6251a587d292b0038c8fc03d05d854979839
Serli30 <tiaa14596@gmail.com>
2023-11-17 22:06:23 +0800

    Day40_NilaiRata-rata

--- Day39_Arrays.java
+++ Day39_Arrays.java
@@ -4,6 +4,7 @@
  */
 package day39_arrays;
 
+import java.io.File;
 import java.util.Scanner;
 
 /**
@@ -16,27 +17,40 @@ public class Day39_Arrays {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    Scanner serli = new Scanner(System.in);
-        int s = serli.nextInt();
-        int[]l =new int[s];
-        int a = 0;
-        int b = 5;
-        
-        for (int i = 0; i < l.length; i++) {
-            if(s % 5 == 0){
-                if(b% 5 == 0){
-                    System.out.println(l[i]+b);
-                    b++;
-            
-        }
-                b++;
-            }else if(s % 5 != 0){
-                a++;
-            if(a % 5 != 0)
-                    System.out.println(l[i]+a);
-            }
-            a++;
+//    Scanner serli = new Scanner(System.in);
+//        int s = serli.nextInt();
+//        int[]l =new int[s];
+//        int a = 0;
+//        int b = 5;
 //        
+//        for (int i = 0; i < l.length; i++) {
+//            if(s % 5 == 0){
+//                if(b% 5 == 0){
+//                    System.out.println(l[i]+b);
+//                    b++;
+//            
+//        }
+//                b++;
+//            }else if(s % 5 != 0){
+//                a++;
+//            if(a % 5 != 0)
+//                    System.out.println(l[i]+a);
+//            }
+//            a++;
+//      
+File file = new File("JavaFile.java");
+    try{
+        boolean value = file.createNewFile();
+        if(value){
+            System.out.println("file baru telah di buat.");
+        }
+        else{
+            System.out.println("file sudah ada");
+        }
     }
+        catch(Exception e){
+        e.getStackTrace( );
+    }
+    
     
     }}
