c06c3499f7b1c4f164e08e0781da33fb2c675f84
Serli30 <tiaa14596@gmail.com>
2023-11-18 23:20:43 +0800

    Day41

---
+++ Day41.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day41;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Day41 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner serli = new Scanner(System.in);
+    int a = serli.nextInt();
+    int sum = 0;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j <= i; j++) {
+                
+            }
+            System.out.println("inputan 1" + i);
+        int j = 0;
+            System.out.println("inputan2 " + j);
+            System.out.println("inputan3 " + i);
+            int b = serli.nextInt();
+            sum+=a;
+            
+        }
+        int avg = sum / sum;
+        System.out.println("average" + avg);
+        System.out.println("sum" + sum);
+    }
+    
+}
