011de36a9dcd005fe13cb56bf046e768c6816819
Serli30 <tiaa14596@gmail.com>
2023-11-25 22:17:11 +0800

    Day48_Method

---
+++ Day48_Method.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package day48_method;
+
+public class Day48_Method {
+    public static void main(String[] args) {
+     int bilangan1= 28;
+     int bilangan2= 07;
+     int hasilduabilangan = penjumlahanduabilangan(bilangan1,bilangan2);
+        System.out.println(bilangan1 +"+"+bilangan2+"="+hasilduabilangan);
+    }
+        public static int penjumlahanduabilangan(int angka1, int angka2){
+        int hasil = angka1+angka2;
+        return hasil;
+    }
+    
+}
